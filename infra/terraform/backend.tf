c296b0cef25e0c154e0ae7bccd688c8a22854863
bximbo <israelabimbolaa@gmail.com>
2025-12-01 13:03:35 +0100

    Fix

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -5,15 +5,4 @@ terraform {
     region         = "eu-north-1"
     encrypt        = true
   }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
 }
